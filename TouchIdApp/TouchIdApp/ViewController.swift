f910da66d41058e0dc157d0aeefad1997b705f52
Ananth <agirijak@in.ibm.com>
2018-05-27 23:28:39 +0530

    initial commit

---
+++ TouchIdApp/TouchIdApp/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  TouchIdApp
+//
+//  Created by Anantha Krishnan K G on 27/05/18.
+//  Copyright © 2018 Ananth. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    @IBOutlet var successLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func touchIdAction(_ sender: UIButton) {
+        
+        print("hello there!.. You have clicked the touch ID")
+        
+        let myContext = LAContext()
+        let myLocalizedReasonString = "Biometric Authntication testing !! "
+        
+        var authError: NSError?
+        if #available(iOS 8.0, macOS 10.12.1, *) {
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+                    
+                    DispatchQueue.main.async {
+                        if success {
+                            // User authenticated successfully, take appropriate action
+                            self.successLabel.text = "Awesome!!... User authenticated successfully"
+                        } else {
+                            // User did not authenticate successfully, look at error and take appropriate action
+                            self.successLabel.text = "Sorry!!... User did not authenticate successfully"
+                        }
+                    }
+                }
+            } else {
+                // Could not evaluate policy; look at authError and present an appropriate message to user
+                successLabel.text = "Sorry!!.. Could not evaluate policy."
+            }
+        } else {
+            // Fallback on earlier versions
+            
+            successLabel.text = "Ooops!!.. This feature is not supported."
+        }
+        
+        
+    }
+}
+
